901286bd5fdee01cdbeb161ae6dd450bbb53e15d
Osvaldo Laini <osvaldolaini@hotmail.com>
2022-10-23 13:47:35 -0300

    master

--- resources/views/components/button-sign.blade.php
+++ resources/views/components/button-sign.blade.php
@@ -1,7 +1,7 @@
 <div class="flex space-x-2 justify-center py-1" >
-    <button-socialite
+    <button
         {{ $attributes->merge([
-            'type' => 'button',
+            'type' => 'submit',
             'data-mdb-ripple'=>'true',
             'data-mdb-ripple-color'=>'light',
             'class'=>'mb-2 mt-1.5 w-full inline-block px-6 py-3 cursor-pointer
@@ -9,9 +9,9 @@
             uppercase rounded shadow-md font-bold
             hover:py-2.5 hover:border-2 hover:border-blue-600 hover:bg-white hover:text-blue-600
             focus:border-blue-600 focus:text-blue-600 focus:outline-none focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out',
+            active:bg-white active:shadow-lg transition duration-150 ease-in-out',
             ])
         }}>
     {{ $slot }}
-    </button-socialite>
+    </button>
 </div>
